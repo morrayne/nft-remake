52b6b7d007157187a5486b2ecad773327def0641
redmorrayne <redmorrayne@gmail.com>
2025-01-23 03:51:57 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,56 +1,3 @@
-const imgScr = [
-  "./res/img/icons/BlueStones.png",
-  "./res/img/icons/GreenBubbles.png",
-  "./res/img/icons/RedWaves.png",
-  "./res/img/icons/WhiteField.png",
-  "./res/img/icons/YellowSpot.png",
-];
-const nameArr = ["Alex", "Lamar", "Kevin", "George", "Herta"];
-const surArr = ["Ca", "Luis", "Washington", "Bright", "White"];
-const numArr = ["120", "260", "300", "470", "510", "690", "730", "890"];
-const list = document.querySelector(".follow-list");
-
-for (let i = 0; i < 25; i++) {
-  const input = createFollow();
-  list.appendChild(input);
-}
-
-function createFollow() {
-  const line = document.createElement("div");
-  line.classList.add("follow-line");
-  const img = document.createElement("img");
-  img.setAttribute("src", imgScr[Math.floor(Math.random() * imgScr.length)]);
-  img.classList.add("follow-img");
-  const followDiv = document.createElement("div");
-  followDiv.classList.add("follow-div");
-  const spanText = document.createElement("span");
-  spanText.classList.add("t");
-  spanText.textContent = nameArr[Math.floor(Math.random() * nameArr.length)];
-  const spanText1 = document.createElement("span");
-  spanText1.textContent = surArr[Math.floor(Math.random() * surArr.length)];
-  spanText1.classList.add("t");
-  const finText = document.createElement("span");
-  finText.textContent = spanText.textContent + " " + spanText1.textContent;
-  finText.classList.add("t");
-  const finNumber = document.createElement("span");
-  finNumber.textContent = numArr[Math.floor(Math.random() * numArr.length)];
-  finNumber.classList.add("t");
-  const btn = document.createElement("button");
-  btn.classList.add("follow-btn");
-  btn.textContent = "follow";
-  btn.classList.add("t");
-
-  line.appendChild(img);
-  followDiv.appendChild(finText);
-  followDiv.appendChild(finNumber);
-  followDiv.children[0].classList.add("t-16");
-  followDiv.children[1].classList.add("t-14");
-  line.appendChild(followDiv);
-  line.appendChild(btn);
-
-  return line;
-}
-
 document.querySelector(".f-list").addEventListener("click", function (event) {
   if (event.target.classList.contains("follow-btn")) {
     if (event.target.classList.contains("followed")) {
@@ -65,52 +12,59 @@ document.querySelector(".f-list").addEventListener("click", function (event) {
   }
 });
 
-const ina = document.querySelector(".explore-holder");
-for (let i = 0; i < 7; i++) {
-  const input = createHolder();
-  ina.appendChild(input);
-}
 
-function createHolder() {
-  const holder = document.createElement("div");
-  holder.classList.add("holder");
-  const img = document.createElement("img");
-  img.setAttribute("src", imgScr[Math.floor(Math.random() * imgScr.length)]);
-  img.classList.add("holder-img");
-  const h3 = document.createElement("span");
-  h3.classList.add("t-20");
-  h3.textContent = "sun glasses";
-  h3.classList.add("t");
-  const holderb = document.createElement("div");
-  holderb.classList.add("holder-b");
-  const hl = document.createElement("div");
-  hl.classList.add("h-l");
-  const sp = document.createElement("span");
-  sp.textContent = "current bid";
-  sp.classList.add("t");
-  sp.classList.add("t-14");
-  const gemdiv = document.createElement("div");
-  gemdiv.classList.add("gem");
-  const gemsvg = document.createElement("img");
-  gemsvg.setAttribute("src", "./res/svg/gem.svg");
-  const num = document.createElement("span");
-  num.textContent = numArr[Math.floor(Math.random() * numArr.length)];
-  num.classList.add("t");
-  num.classList.add("t-14");
-  const btn = document.createElement("button");
-  btn.classList.add("b-btn");
-  btn.textContent = "place-bid";
-  btn.classList.add("t");
+const navChildren = document.querySelector(".header-nav").children;
 
-  gemdiv.appendChild(gemsvg);
-  gemdiv.appendChild(num);
-  hl.appendChild(sp);
-  hl.appendChild(gemdiv);
-  holderb.appendChild(hl);
-  holderb.appendChild(btn);
-  holder.appendChild(img);
-  holder.appendChild(h3);
-  holder.appendChild(holderb);
+document.querySelector(".header-nav").addEventListener("click", function(event) {
+  let targetElement
+    if (navChildren[0] === event.target) {
+        targetElement = document.querySelector(".discover");
+    } else if (navChildren[1] === event.target) {
+        targetElement = document.querySelector(".explore");
+    } else if (navChildren[2] === event.target) {
+        targetElement = document.querySelector(".just");
+    } else {
+        targetElement = document.querySelector(".table");
+    }
+    if (targetElement) {
+        const elementPosition = targetElement.getBoundingClientRect().top + window.scrollY;
+        const offsetPosition = elementPosition - 80;
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: "smooth"
+        });
+    }
+});
 
-  return holder;
-}
+
+const imgD = document.querySelector(".d-img-c");
+const images = Array.from(imgD.children); // Получаем массив элементов внутри imgD
+let currentIndex = 0; // Индекс текущего изображения
+const gap = document.querySelector(".d-img").clientWidth; // Ширина одного изображения
+const cont = document.querySelector(".d-point-c");
+
+cont.addEventListener("click", function(event) {
+  const firstChild = cont.children[0];
+  
+  if (firstChild && firstChild.contains(event.target)) {
+    console.log("left");
+    // Уменьшаем индекс, если он больше 0
+    if (currentIndex > 0) {
+      currentIndex--;
+    }
+  } else {
+    console.log("right");
+    // Увеличиваем индекс, если он меньше количества изображений - 1
+    if (currentIndex < images.length - 1) {
+      currentIndex++;
+    }
+  }
+  
+  // Обновляем положение каждого элемента
+  images.forEach((image) => {
+    image.style.transform = "translateX(-" + gap * currentIndex + "px)";
+  });
+});
+
+
+
